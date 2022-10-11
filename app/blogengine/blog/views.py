e294efd26a91eb75e67fc7062b17be10a381182e
TemirbolatAbdulazim <azimtimudolat@gmail.com>
2022-10-11 11:33:46 +0600

    testPage-v1

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def post_list(request):
     return render(request,'blog/index.html')
+
+def test_list(request):
+    return render(request,'blog/testpage.html')
