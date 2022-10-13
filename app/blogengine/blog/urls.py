c0ef8eb3f0bce201b7b74092560addc7ecf1e077
TemirbolatAbdulazim <azimtimudolat@gmail.com>
2022-10-13 21:46:59 +0600

    -v3 reg page

--- app/blogengine/blog/urls.py
+++ app/blogengine/blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('', post_list),
-    path('Createtest', test_list),
+    path('', first_list),
+    path('Createtest', createTest_page),
+    path('singIn', singIn_page),
+    path('singUp', singUp_page),
+    path('home', first_list),
 ]
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 
-def post_list(request):
+def first_list(request):
     return render(request,'blog/index.html')
 
-def test_list(request):
-    return render(request,'blog/testpage.html')
+def createTest_page(request):
+    return render(request,'blog/createtest.html')
+
+def singIn_page(request):
+    return render(request,'blog/singIn.html')
+
+def singUp_page(request):
+    return render(request,'blog/singUp.html')
